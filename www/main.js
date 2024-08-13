e108acfb6481ae335228c21879477383a57c1bc7
Mr Ankit <hxankit01@gmail.com>
2024-08-13 12:21:19 +0530

    first commit

---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn"
+        },
+        out: {
+            effect: "bounceOut"
+        },
+    })
+});
